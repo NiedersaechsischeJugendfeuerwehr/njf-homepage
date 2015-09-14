56b3a04fd66afdc67bec9538f9d82a261e6cc0d4
Markus Lehmann <markus.lehmann@mailbox.org>
2015-09-14 22:03:35 +0200

    German Translations of the wordpress plugin Download Manager added
    Signed-off-by: Markus Lehmann <markus.lehmann@mailbox.org>

---
+++ translations/download-manager/page-template-default.php
@@ -0,0 +1,32 @@
+<!-- WPDM Template: Default Template -->
+<!-- folder url: /download-manager/templates
+<!-- Edited 11.09.2015 by Markus Alexander Lehmann-->
+<!-- markus.lehmann@jf-weser-ems.de-->
+<!-- WPDM Version 2.7.96 -->
+<div class="row">
+<div class="col-md-12">
+[thumb_900x0]
+</div>
+    <div class="col-md-12"><br/>
+<table class="table">
+<tbody>
+<tr><td>Downloads</td><td>[download_count]</td></tr>
+<!-- not necessary, cause all downloads are unlimited
+	//<tr><td>Stock</td><td>[quota]</td></tr> -->
+<tr><td>Dateigröße</td><td>[file_size]</td></tr>
+<tr><td>Erstellungsdatum</td><td>[create_date]</td></tr>
+<tr><td colspan="2">[download_link_extended]</td></tr>
+</tr>
+</tbody></table>
+        </div>
+
+
+<div class="col-md-12">
+[description]  
+ 
+
+
+</div> 
+</div>
+
+
